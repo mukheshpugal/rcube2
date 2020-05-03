6f48ca702caf68517c9e4e549a2843c1ce3c46cd
Srivenkat <srivenkatanr@gmail.com>
2020-05-03 11:51:45 +0530

    Updated extract feature class

--- core/cube_feature_extract_class.py
+++ core/cube_feature_extract_class.py
@@ -14,35 +14,35 @@ YELLOW = (0,255,255)
    
 cwd = os.getcwd()
 
-class cube_extract(object):
+class featureExtract(object):
     def __init__(self,imagelist):
         self.img = [None,None,None,None,None,None]
         self.grid = [None,None,None,None,None,None]
         self.face = [None,None,None,None,None,None]
         self.stickers = [None,None,None,None,None,None]
-        self.ccount = [0,0,0,0,0,0]
-        self.carray = []
+        self.Ccount = [0,0,0,0,0,0]
+        self.Carray = []
         self.Cdict = []
-        self.pixelmap = []
+        self.pixelMap = []
         for k in range(len(imagelist)):
-            self.img[k] = self.process_img(imagelist[k])
-            self.face_extract(k)
+            self.img[k] = self.processImg(imagelist[k])
+            self.faceExtract(k)
         self.cluster()
         for k in range(len(imagelist)):
-            colormat = self.colormap(self.pixelmap[k])
+            colormat = self.colorMap(self.pixelMap[k])
             self.update(colormat,k)
-        if any(np.asarray(self.ccount) != 9):
+        if any(np.asarray(self.Ccount) != 9):
             print("Wrong sticker outputs!!")
-            print(self.ccount)
+            print(self.Ccount)
     
-    def process_img(self,img):
+    def processImg(self,img):
         if img.shape[1] > img.shape[0]:
             img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)            
         return img
         
-    def face_extract(self,k):
+    def faceExtract(self,k):
         temp = self.img[k].copy()
-        areas,rect_contours,img_area,cube_area = self.cntprocess(temp,[0])
+        areas,rect_contours,img_area,cube_area = self.cntProcess(temp,[0])
         rect = []
         for i in range(len(rect_contours)):
             if  areas[i]>img_area/200 and areas[i]<img_area/20:
@@ -70,51 +70,51 @@ class cube_extract(object):
                     img2 = temp.copy()
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     h,s,v = cv2.split(img2)                    
-                    areas,rect_contours,img_area,cube_area = self.cntprocess(img2,v)
+                    areas,rect_contours,img_area,cube_area = self.cntProcess(img2,v)
                     m+=1
             elif m == 2:
                     img2 = temp.copy()
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     h,s,v = cv2.split(img2)
-                    areas,rect_contours,img_area,cube_area = self.cntprocess(img2,s)
+                    areas,rect_contours,img_area,cube_area = self.cntProcess(img2,s)
                     m+=1
             elif m == 3:
                     img2 = temp.copy()
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     h,s,v = cv2.split(img2)
-                    areas,rect_contours,img_area,cube_area = self.cntprocess(img2,h)
+                    areas,rect_contours,img_area,cube_area = self.cntProcess(img2,h)
                     m+=1
             elif m == 4:
                     img2 = temp.copy()
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     h,s,v = cv2.split(img2)
-                    areas,rect_contours,img_area,cube_area = self.cntprocess(img2,v)
+                    areas,rect_contours,img_area,cube_area = self.cntProcess(img2,v)
                     m+=1
             elif m == 5:
                     img2 = temp.copy()
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     h,s,v = cv2.split(img2)
-                    areas,rect_contours,img_area,cube_area = self.cntprocess(img2,s)
+                    areas,rect_contours,img_area,cube_area = self.cntProcess(img2,s)
                     m+=1
             elif m == 6:
                     img2 = temp.copy()
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
                     h,s,v = cv2.split(img2)
-                    areas,rect_contours,img_area,cube_area = self.cntprocess(img2,h)
+                    areas,rect_contours,img_area,cube_area = self.cntProcess(img2,h)
                     m+=1
             else:
                 break  
-        rect = self.contoursort(rect)
+        rect = self.contourSort(rect)
         self.stickers[k] = cv2.drawContours(self.img[k].copy(),rect,-1,YELLOW,2)
         if len(rect) != 9:
             print("Something's wrong",k,len(rect))   
             print("The image area is",img_area)
-        self.get_faceval(rect,k)
+        self.getFaceVal(rect,k)
     
-    def cntprocess(self,img,channel):
+    def cntProcess(self,img,channel):
         temp = img.copy()
         img_area = temp.shape[0]*temp.shape[1]
         temp = cv2.GaussianBlur(temp,(17,17),0)
@@ -140,7 +140,7 @@ class cube_extract(object):
         return areas,rect_contours,img_area,cube_area   
     
     def cluster(self):
-        colours_list = np.asarray(self.carray)
+        colours_list = np.asarray(self.Carray)
         kmeans = KMeans(n_clusters=6)
         kmeans.fit(colours_list)
         colours = kmeans.cluster_centers_
@@ -157,7 +157,7 @@ class cube_extract(object):
             self.Cdict.append(colours[ind])
         
         
-    def get_faceval(self,rect,k):
+    def getFaceVal(self,rect,k):
         temp = self.img[k].copy()
         sq = np.asarray(rect).reshape(3,3,4,2)
         pixel_b = np.zeros([3,3])
@@ -174,17 +174,17 @@ class cube_extract(object):
                                 sec[y,x,c] = np.clip(1*sec[y,x,c] + 2, 0, 255)
                     sec = cv2.GaussianBlur(sec,(25,25),0)
                     pixel_b[i,j],pixel_g[i,j],pixel_r[i,j] = sec[12,12]
-                    self.carray.append(sec[12,12])                                                     
-        self.pixelmap.append([pixel_b,pixel_g,pixel_r])
+                    self.Carray.append(sec[12,12])                                                     
+        self.pixelMap.append([pixel_b,pixel_g,pixel_r])
 
-    def contoursort(self,rect):
+    def contourSort(self,rect):
         rect = sorted(rect,key=lambda b:b[1][1],reverse=False)
         rect[0:3] = sorted(rect[0:3],key=lambda b:b[1][0],reverse=False)
         rect[3:6] = sorted(rect[3:6],key=lambda b:b[1][0],reverse=False)
         rect[6:9] = sorted(rect[6:9],key=lambda b:b[1][0],reverse=False)
         return rect  
         
-    def colormap(self,colors):
+    def colorMap(self,colors):
         mat = np.zeros([3,3])
         B,G,R = colors
         col = self.Cdict
@@ -206,21 +206,21 @@ class cube_extract(object):
             for j in range(3):
                 if mat[i,j] == 0:
                     self.face[k][i,j] = WHITE
-                    self.ccount[0] += 1
+                    self.Ccount[0] += 1
                 elif mat[i,j] == 1:
                     self.face[k][i,j] = ORANGE
-                    self.ccount[1] += 1
+                    self.Ccount[1] += 1
                 elif mat[i,j] == 2:
                     self.face[k][i,j] = GREEN
-                    self.ccount[2] += 1
+                    self.Ccount[2] += 1
                 elif mat[i,j] == 3:
                     self.face[k][i,j] = RED
-                    self.ccount[3] += 1
+                    self.Ccount[3] += 1
                 elif mat[i,j] == 4:
                     self.face[k][i,j] = BLUE
-                    self.ccount[4] += 1
+                    self.Ccount[4] += 1
                 elif mat[i,j] == 5:
                     self.face[k][i,j] = YELLOW
-                    self.ccount[5] += 1
+                    self.Ccount[5] += 1
                 else:
                     pass              
