16a0aeb89f10b0cd6e9b3af43c9d68823cfaeb96
Srivenkat <srivenkatanr@gmail.com>
2020-05-04 16:46:46 +0530

    Added file io to match environments

---
+++ Extract_stickers.py
@@ -0,0 +1,23 @@
+import numpy as np
+try:
+	import cv2
+except:
+	import cv2
+from core.cube_feature import featureExtract
+
+side0 = cv2.imread("images/test-2/image1.jpeg")
+side1 = cv2.imread("images/test-2/image2.jpeg")
+side2 = cv2.imread("images/test-2/image3.jpeg")
+side3 = cv2.imread("images/test-2/image4.jpeg")
+side4 = cv2.imread("images/test-2/image5.jpeg")
+side5 = cv2.imread("images/test-2/image6.jpeg")
+
+cube_ext = featureExtract([side0,side1,side2,side3,side4,side5])
+
+sides = cube_ext.grid
+
+i=0
+for side in sides:
+	np.savetxt("temp/side{}.txt".format(i),side)
+	i+=1
+
--- game3D.py
+++ game3D.py
@@ -7,29 +7,24 @@ Created on Sun May  3 23:55:09 2020
 """
 from core.cube3D_class import cubeProjection
 from core.wireframe import Wireframe
-from core.cube_feature import featureExtract
-import numpy as np 
-import cv2
+import numpy as np
 
 colors = [(255, 255, 255), (255,255,0), (0,255,0), (0,0,255), (255,0,0), (255,128,0), (0, 0, 0)]
+faces = []
+for i in range(6):
+	side = np.loadtxt("temp/side{}.txt".format(i))
+	faces.append(np.uint8(side))
 
-face0 = cv2.imread("images/solved/image1.jpeg")
-face1 = cv2.imread("images/solved/image2.jpeg")
-face2 = cv2.imread("images/solved/image3.jpeg")
-face3 = cv2.imread("images/solved/image4.jpeg")
-face4 = cv2.imread("images/solved/image5.jpeg")
-face5 = cv2.imread("images/solved/image6.jpeg")
-
-cube_ext = featureExtract([face0,face1,face2,face3,face4,face5])
-faces = cube_ext.face
-
-#faces = [i*np.ones((3,3)) for i in range(6)]
+new_faces = [None,None,None,None,None,None]
+for i in range(len(faces)):
+    j = faces[i][1][1]
+    new_faces[j] = faces[i]
 
 cube_nodes = [(x,y,z) for x in range(-3,4,2) for y in range(-3,4,2) for z in range(-3,4,2)]
 
 cube = Wireframe()
 cube.initNodeList(cube_nodes)
-cube.initFaces(faces)
+cube.initFaces(new_faces)
 
 cp = cubeProjection(1000,1000)
 cp.addWireframe('cube',cube)
